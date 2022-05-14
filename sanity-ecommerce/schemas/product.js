af2bb3e08f4b136541d07fa1ed6c8f34592fdde6
Antonio Lopez <pezlop202@gmail.com>
2022-05-14 11:46:40 -0700

    sanity io initial layout

---
+++ sanity-ecommerce/schemas/product.js
@@ -0,0 +1,70 @@
+export default {
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  initialValue: {
+    au: 'N/A',
+    eu: 'N/A',
+    jp: 'N/A',
+    na: 'N/A',
+  },
+  fields: [
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'array',
+      of: [{ type: 'image' }],
+      options: { hotspot: true },
+    },
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'character',
+      title: 'Character',
+      type: 'string',
+    },
+    {
+      name: 'amiiboSeries',
+      title: 'Amiibo Series',
+      type: 'string',
+    },
+    {
+      name: 'au',
+      title: 'AU Release',
+      type: 'string',
+    },
+    {
+      name: 'eu',
+      title: 'EU Release',
+      type: 'string',
+    },
+    {
+      name: 'jp',
+      title: 'JP Release',
+      type: 'string',
+    },
+    {
+      name: 'na',
+      title: 'NA Release',
+      type: 'string',
+    },
+    {
+      name: 'id',
+      title: 'ID',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'id',
+        maxLength: 90,
+      },
+    },
+    { name: 'price', title: 'Price', type: 'number' },
+  ],
+};
