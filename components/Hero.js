7c3a0782b8659fc7da07362c8b072934c733ae54
Antonio Lopez <pezlop202@gmail.com>
2022-05-18 19:42:11 -0700

    update hero

--- components/Hero.js
+++ components/Hero.js
@@ -3,12 +3,14 @@ import React from 'react';
 const Hero = () => {
   return (
     <div className='relative mx-auto max-w-screen-2xl'>
-      <div className='absolute z-10 flex justify-center items-end w-full pt-2'>
-        <span className='text-white text-xl'>Amiibo Hub</span>
+      <div className='absolute z-10 flex w-full items-end justify-center pt-3'>
+        <span className='p-2 text-4xl font-black text-white outline'>
+          Amiibo Hub
+        </span>
       </div>
       <div className='relative'>
-        <div className='bg-black opacity-30 w-full h-full absolute' />
-        <img src='/images/hero-image.png' alt='hero' />
+        <div className='absolute h-full w-full bg-black opacity-30' />
+        <img src='/images/amiibo-hero.webp' alt='hero' />
       </div>
     </div>
   );
